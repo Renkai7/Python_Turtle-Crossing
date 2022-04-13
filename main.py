99bb0359fdfaa6339ffe3337880061687ceaed7e
Mike <ms53150@gmail.com>
2022-04-12 20:56:09 -0400

    Add methods to car manager to spawn random number of cars and move along X-axis

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    # Detect player crossing finish line
+    if player.ycor() > 300:
+        game_is_on = False
+
+screen.exitonclick()
